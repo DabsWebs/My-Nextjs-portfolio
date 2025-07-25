bd1947fe4e90fbd688fb2577b0f4cb26d6907f50
DabsWebs <confydenz@gmail.com>
2025-07-25 05:44:44 +0100

    components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-
-
 export default function Home() {
   return (
-   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10" >
-    <div>
-      <h1>Hello, Portfolio </h1>
-    </div>
-   </main>
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+      <div className="max-w-7x1 w-full">
+        <h1>Hello, Portfolio </h1>
+      </div>
+    </main>
   );
 }
