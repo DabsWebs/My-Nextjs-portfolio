7a195afd78e9956cd303da31b2952cd728ea3faa
DabsWebs <confydenz@gmail.com>
2025-07-25 13:30:20 +0100

    provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="max-w-7x1 w-full">
-        <h1>Hello, Portfolio </h1>
+        <Hero />
       </div>
     </main>
   );
--- postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-// postcss.config.js or postcss.config.mjs
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-
