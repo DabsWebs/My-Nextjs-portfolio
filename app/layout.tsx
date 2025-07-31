d8744e6b2ef0df39095d93ea6ec41d6e2a735e5b
DabsWebs <confydenz@gmail.com>
2025-07-31 03:04:12 +0100

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "./provider";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from './provider';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
   title: " Daberechi's Portfolio ",
-  description: "Recent and minimalist DabsWebs portfolio",
+  description: 'Recent and minimalist DabsWebs portfolio',
 };
 
 export default function RootLayout({
@@ -28,17 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-  <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-
-
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,9 @@
 // ./ui/BentoGrid.tsx
 import { cn } from '@/lib/utils';
+import { BackgroundGradientAnimation } from './GradientBg';
+import { Globe } from './Globe';
+import { GlobeDemo } from './GridGlobe';
+import { div, span } from 'framer-motion/client';
 
 export const BentoGrid = ({
   className,
@@ -24,32 +28,96 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
   id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
-  id: number;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName: string;
+  spareImg?: string;
 }) => {
   return (
     <div
       className={cn(
-        'group/bento shadow-input row-span-1 relative flex flex-col justify-between space-y-4 rounded-3xl border bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none border-red-500',
+        'group/bento shadow-input row-span-1 relative overflow-hidden flex flex-col justify-between space-y-4 rounded-3xl border bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none ',
         className
       )}
+      style={{
+        background: '#050536',
+        backgroundColor:
+          'linear-gradient(90deg, rgba(4, 7, 29, 1) 0%, rgba(52, 69, 65, 1) 0%, rgba(12, 14, 35, 1) 0%)',
+      }}
     >
-      {header}
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {icon}
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
+      <div className={`${id === 6} && 'flex justify-center h-full `}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, 'object-cover, object-center')}
+            />
+          )}
         </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
+
+        <div
+          className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className={'object-cover, object-center w-full h-full'}
+            />
+          )}
+        </div>
+        <div>
+          {id === 6 && (
+            <BackgroundGradientAnimation>
+              <div className="abdolute z-50 flex items-center justify-center text-white font-bold " />
+            </BackgroundGradientAnimation>
+          )}
+
+          <div
+            className={cn(
+              titleClassName,
+              'group-hover/ bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 '
+            )}
+          >
+            <div className="font-sans text-sm font-extralight text-[#c1c2d3] md:text-xs lg:text-base z-10 ">
+              {description}
+            </div>
+            <div className=" font-sans font-bold text-lg lg:text-3xl max-w-96 z-10 ">
+              {title}
+            </div>
+          </div>
+
+          <div>
+            {id === 2 && <GlobeDemo />}
+            {id === 3 && (
+              <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 ">
+                <div className="flex flex-col gap-3 lg:gap-8">
+                  {' '}
+                  {['React.js', 'Next.js', 'TypeScript'].map(item => (
+                    <span
+                      key={item}
+                      className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] "
+                    >
+                      {item}
+                    </span>
+                  ))}{' '}
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
